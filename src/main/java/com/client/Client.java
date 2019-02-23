33f6e4ee23c706a3a1f729a82e82387227505e0d
Calin <caling@pm.me>
2019-02-23 14:21:00 +0100

    Adding the server, the client and the dependencies to pom.xml

--- src/main/java/com/client/Client.java
+++ src/main/java/com/client/Client.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
 public class Client {
     public static void main(String[] args) {
         try {
