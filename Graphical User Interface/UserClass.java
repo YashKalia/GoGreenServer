3dd106241dbd7f63273c129ff46ab55e7d759fb0
Pradhyumnaa Ganapathi Subramanian <p.ganapathisubramanian@student.tudelft.nl>
2019-03-11 15:21:25 +0000

    Upload New File

---
+++ Graphical User Interface/UserClass.java
@@ -0,0 +1,29 @@
+package application;
+
+public class UserClass {
+	
+	public String Username;
+	public String Password;
+	
+	
+	public UserClass(String name, String PWord) {
+		Username = name;
+		Password = PWord;
+		
+	}
+	
+	public String getuser() {
+		return Username;
+	}
+	public String getpassword() {
+		return Password;
+	}
+	
+	
+	
+	
+
+
+
+}
+
