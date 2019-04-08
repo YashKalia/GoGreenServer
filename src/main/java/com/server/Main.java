0bff99678c208d2311c7f227dc07c2c614f74b43
Francine Biazin do Nascimento <F.BiazindoNascimento@student.tudelft.nl>
2019-04-08 17:09:17 +0200

    Add edited Main

--- src/main/java/com/server/Main.java
+++ src/main/java/com/server/Main.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
     public static void main(String[] args) {
-        Connect.retrieveData("result_food_meat");
         SpringApplication.run(Main.class, args);
     }
 
