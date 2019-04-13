670c369c8523f807fe997920a2902f101d7bbbf0
Dorka <d.a.hevizi-1@student.tudelft.nl>
2019-04-13 14:18:26 +0200

    checkstyle

--- src/test/java/com/server/api/CalculatorTest.java
+++ src/test/java/com/server/api/CalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
+
 public class CalculatorTest {
 
     Calculator calc;
@@ -21,8 +22,8 @@ public class CalculatorTest {
         double coEmission = 0;
         try {
             Float co = Connect.retrieveData("result_food_meat");    //getting the data from the API
-            double d = co.doubleValue();    //converting float value to double
-            coEmission = d * 1000;
+            double value= co.doubleValue();    //converting float value to double
+            coEmission = value * 1000;
             coEmission = coEmission / 365;   //breaking it down to a meal a day and to kgs instead of tons
         } catch (NullPointerException e) {
             e.printStackTrace();
